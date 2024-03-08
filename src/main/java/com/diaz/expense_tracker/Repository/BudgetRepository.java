0ffb1c72d7d52cbb4c5d1938c16d56ff740bbaf8
Devin Diaz <diazdevin7@outlook.com>
2024-03-07 19:56:54 -0500

    added annotations to repository classes

--- src/main/java/com/diaz/expense_tracker/Repository/BudgetRepository.java
+++ src/main/java/com/diaz/expense_tracker/Repository/BudgetRepository.java
@@ -2,6 +2,8 @@ package com.diaz.expense_tracker.Repository;
 
 import com.diaz.expense_tracker.model.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
 }
--- src/main/java/com/diaz/expense_tracker/Repository/ExpenseRepository.java
+++ src/main/java/com/diaz/expense_tracker/Repository/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.diaz.expense_tracker.Repository;
 
 import com.diaz.expense_tracker.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
