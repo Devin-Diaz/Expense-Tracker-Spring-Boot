518f3d92ffd9ca4b68105e49bf85431fc2817ddc
Devin Diaz <diazdevin7@outlook.com>
2024-03-14 22:46:59 -0400

    made custom UserDetailsService class so when servlet intercepts request, it fetches data from database (email & passwd) for future comparisions before logging in

--- src/main/java/com/diaz/expense_tracker/security/CustomUserDetailsService.java
+++ src/main/java/com/diaz/expense_tracker/security/CustomUserDetailsService.java
@@ -1,2 +1,47 @@
-package com.diaz.expense_tracker.security;public class CustomUserDetailsService {
+package com.diaz.expense_tracker.security;
+
+import com.diaz.expense_tracker.Repository.UserRepository;
+import com.diaz.expense_tracker.model.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.Collections;
+
+
+/*
+    Let's use an analogy for this class
+    We have a library system where to borrow books users have a library card.
+
+    UserDetails(interface) is the library card
+
+    CustomUserDetails(class) is you the librarian. Responsible for verifying library members based on their cards
+
+    loadByUsername - the process of looking up a members details in your records to verify identity.
+    In this case we look them up via email
+    return statement - this is like confirming the members details and allowing them to access library's
+    resources based on their membership level
+
+*/
+
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    // verifies users role and acknowledges them so they can login
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+
 }
