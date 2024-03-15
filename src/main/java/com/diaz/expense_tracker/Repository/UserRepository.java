202e381859f5a204d52afc4daf0dfe391f536b8f
Devin Diaz <diazdevin7@outlook.com>
2024-03-14 22:26:28 -0400

    added findByEmail jpa helper method for user authentication w/ spring security via CustomUserDetailsService class

--- src/main/java/com/diaz/expense_tracker/Repository/UserRepository.java
+++ src/main/java/com/diaz/expense_tracker/Repository/UserRepository.java
@@ -4,6 +4,13 @@ import com.diaz.expense_tracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //helper jpa method for our CustomUsersDetailsService class
+    //will be used to help authenticate users when logging in to webapp
+    Optional<User> findByEmail(String email);
+    
 }
