51f6b9a5bbb27c55bf4498f0fd301be6a5716cd8
Devin Diaz <diazdevin7@outlook.com>
2024-03-17 22:46:14 -0400

    adjusting dashboard mapping

--- src/main/java/com/diaz/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/diaz/expense_tracker/controller/ExpenseController.java
@@ -20,16 +20,16 @@ public class ExpenseController {
 
 
 
-    @GetMapping("/dashboard")
+    @GetMapping("/db")
     public String showDashboard(Model model, Principal principal) {
         // Optional: Use the principal to fetch user-specific data
         String email = principal.getName(); // This gets the username (email in your case)
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-
+        model.addAttribute("user", user);
         // Load data needed for the dashboard. For example, user's tasks, profile info, etc.
         // model.addAttribute("tasks", tasksService.getTasksForUser(user.getId()));
 
-        return "dashboard"; // Return the view for the dashboard
+        return "db"; // Return the view for the dashboard
     }
 
 
